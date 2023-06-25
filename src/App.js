18513c3f37b09463acc76d8434a4cfa697c99d2c
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-06-26 00:34:12 +0600

    website name set & favicon added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
-
       <button className="btn btn-outline">Default</button>
       <button className="btn btn-outline btn-primary">Primary</button>
       <button className="btn btn-outline btn-secondary">Secondary</button>
