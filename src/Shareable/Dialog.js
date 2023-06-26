f160af5b163936b60a975fae0c74f28ca82eb6d9
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-06-27 01:45:12 +0600

    profile modal done with links

--- src/Shareable/Dialog.js
+++ src/Shareable/Dialog.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import kaniz from '../Assests/kaniz.jpg'
 
 const Dialog = () => {
     return (
 
         <div>
-            < button className="btn" onClick={() => window.my_modal_5.showModal()}>
-                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" alt='' />
+            < button  onClick={() => window.my_modal_5.showModal()}>
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar" >
+                    <div className=" rounded-full">
+                        <img src={kaniz} alt='' />
+                    </div>
+                </label>
             </button >
             <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
                 <form method="dialog" className="modal-box">
-                    <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">Press ESC key or click the button below to close</p>
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    <h3 className="font-bold text-lg">Assalamualaikum !!</h3>
+                    <p className="py-2">"My name is "Kaniz Fatima".I am a MERN stack web developer. I love to use my creativity and make something new. That's why I love to play with code. Over the last year, I gather knowledge in various parts of web development by myself. My ultimate goal is to become an awesome full-stack web developer who can make beautiful UIs and also can handle the back-end smoothly."</p>
+                    <img src={kaniz} alt='' />
                     <div className="modal-action">
                         {/* if there is a button in form, it will close the modal */}
-                        <button className="btn">Close</button>
+                        <button className="btn"><a target="_blank" href="https://www.linkedin.com/in/kaniz-mitu/">linkedin</a></button>
+                        <button className="btn"><a target="_blank" href="https://www.linkedin.com/in/kaniz-mitu/">Github</a></button>
                     </div>
                 </form>
             </dialog>
--- src/Shareable/Navbar.js
+++ src/Shareable/Navbar.js
@@ -62,13 +62,7 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar" >
-                        <div className="w-10 rounded-full">
-                            {/* <img  src="/images/stock/photo-1534528741775-53994a69daeb.jpg" alt=''/> */}
-                            <Dialog></Dialog>
-                        </div>
-                    </label>
-                
+                    <Dialog></Dialog>
                 </div>
             </div>
         </div>
