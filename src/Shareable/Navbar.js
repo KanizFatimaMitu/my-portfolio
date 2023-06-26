9f2b06e77d4f909f377a3eae04ce91a9c0a29397
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-06-26 23:48:57 +0600

    all issues fixed in navbar

--- src/Shareable/Navbar.js
+++ src/Shareable/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -12,57 +13,57 @@ const Navbar = () => {
                         {/* menu */}
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                             <li>
-                                <a>
+                                <Link>
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
                                     Inbox
                                     <span className="badge badge-sm">99+</span>
-                                </a>
+                                </Link>
                             </li>
                             <li>
-                                <a>
+                                <Link>
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                     Updates
                                     <span className="badge badge-sm badge-warning">NEW</span>
-                                </a>
+                                </Link>
                             </li>
                             <li>
-                                <a>
+                                <Link>
                                     Stats
                                     <span className="badge badge-xs badge-info"></span>
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <Link className="btn btn-ghost normal-case text-xl" >daisyUI</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <li>
-                            <a>
+                            <Link>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
                                 Inbox
                                 <span className="badge badge-sm">99+</span>
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a>
+                            <Link>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                 Updates
                                 <span className="badge badge-sm badge-warning">NEW</span>
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a>
+                            <Link>
                                 Stats
                                 <span className="badge badge-xs badge-info"></span>
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
                 <div className="navbar-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
-                            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" alt=''/>
                         </div>
                     </label>
                 </div>
