488c4b56a8dc76eb17c0386d4c4ef7705c023dec
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-06-28 00:30:32 +0600

    font awesome installed and used

--- src/Shareable/Navbar.js
+++ src/Shareable/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import Dialog from './Dialog';
 
 const Navbar = () => {
@@ -29,7 +31,8 @@ const Navbar = () => {
                             </li>
                             <li>
                                 <Link to='/about'>
-                                  About Me
+                                    <FontAwesomeIcon icon={faCoffee} />
+                                    About Me
                                     <span className="badge badge-xs badge-info"></span>
                                 </Link>
                             </li>
@@ -40,22 +43,23 @@ const Navbar = () => {
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <li>
-                        <Link to='/'>
+                            <Link to='/'>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
                                 Home
                                 <span className="badge badge-sm">99+</span>
                             </Link>
                         </li>
                         <li>
-                        <Link to='/project'>
+                            <Link to='/project'>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                 My Work
                                 <span className="badge badge-sm badge-warning">NEW</span>
                             </Link>
                         </li>
                         <li>
-                        <Link to='/about'>
-                               About ME
+                            <Link to='/about'>
+                                <FontAwesomeIcon icon={faCoffee} />
+                                About ME
                                 <span className="badge badge-xs badge-info"></span>
                             </Link>
                         </li>
