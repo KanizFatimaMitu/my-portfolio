19eb340a318143b59f297066758c565ea5962fd3
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-06-28 00:36:57 +0600

    aos not worked

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Intro = () => {
     return (
-        <div>
+        <div >
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content text-center">
-                    <div className="max-w-md">
+                    <div
+                        className="max-w-md"
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out">
                         <h1 className="text-5xl font-bold">Hello there</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         <button className="btn btn-primary">Get Started</button>
