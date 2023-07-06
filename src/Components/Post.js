ef4f32bc2f2c9bead1918ba7a86220282a513058
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-07-07 02:51:58 +0600

    organized perfectly

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
     return (
         <div >
-                    <div className="card w-auto bg-base-100 shadow-xl">
-                        <div className="card-body">
-                            <h2 className="card-title">{post.title}</h2>
-                            <p>{post.body}</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
-                            </div>
-                        </div>
+            <div className="card bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src={post.thumbnailUrl} alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{(post.title).slice(0,25)}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
-    );
+            </div>
+            </div>
+            );
 };
 
-export default Post;
+            export default Post;
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -3,53 +3,60 @@ import Post from '../Components/Post';
 
 
 const Projects = () => {
-    const [posts, setPosts] = useState([])
-    const [page, setPage] = useState(1)
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts")
+        fetch("https://jsonplaceholder.typicode.com/photos?limit=50")
             .then(res => res.json())
             .then(data => setPosts(data))
 
     }, [])
 
     const pageHandler = (select) => {
-        if (select >= 1 && select <= posts.length / 10 && select !== page)
+        if (select >= 1
+            && select <= posts.length / 10
+            && select !== page)
             setPage(select)
     }
 
     return (
         <div>
+            <div className='grid grid-cols-1 lg:grid-cols-4 gap-4 p-4'>
 
-            {
-                posts.slice(page * 10 - 10, page * 10).map(post =>
-                    <div className='grid grid-cols-4 gap-4 p-4'>
+                {
+                    posts.slice(page * 10 - 10, page * 10).map(post =>
+
                         <Post
                             post={post}
                             key={post.id}
                         ></Post>
-                    </div>)
-            }
-            {
-                posts.length > 0 && <div class="join">
-                    <button
-                        onClick={() => pageHandler(page - 1)}
-                        class="join-item btn">«</button>
-                    {
-                        [...Array(posts.length / 10)].map((_, i) => {
-                            return <button
-                                onClick={() => pageHandler(i + 1)}
-                                className={page === (i + 1) ? "join-item btn " : "join-item btn btn-active"}>
-                                Page {i + 1}
-                            </button>
-                        })
-                    }
-                    <button
-                        onClick={() => pageHandler(page + 1)}
-                        class="join-item btn">»</button>
-                </div>
-            }
+                    )
+                }
 
+
+            </div>
+            <div>
+                {
+                    posts.length > 0 && <div class="join">
+                        <button
+                            onClick={() => pageHandler(page - 1)}
+                            class="join-item btn">«</button>
+                        {
+                            [...Array(posts.length / 10)].map((_, i) => {
+                                return <button
+                                    onClick={() => pageHandler(i + 1)}
+                                    className={page === (i + 1) ? "join-item btn " : "join-item btn btn-active"}>
+                                    Page {i + 1}
+                                </button>
+                            })
+                        }
+                        <button
+                            onClick={() => pageHandler(page + 1)}
+                            class="join-item btn">»</button>
+                    </div>
+                }
+            </div>
         </div>
     );
 };
