db1adf5aa307cf5836d6571477eb554364d1deb2
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-07-06 23:28:46 +0600

    fetch all data

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Shareable/Navbar';
 import Footer from '../Shareable/Footer';
 
 const Projects = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(res => res.json())
+            .then(data => setPosts(data))
+
+    }, [])
+
     return (
         <div>
             <Navbar></Navbar>
-            <h1>All projects will shown here</h1>
+            {
+                posts.map(post => <div className='grid grid-cols-1 lg:grid-cols-3 gap-2'>
+                    <div className="card bg-base-100 shadow-xl">
+                        <div className="card-body">
+                            <h2 className="card-title">{post.title}</h2>
+                            <p>{post.body}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>)
+            }
             <Footer></Footer>
         </div>
     );
