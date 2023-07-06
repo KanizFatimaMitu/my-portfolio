a81dbdb7db365142499a6db53fc942564f17e378
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-07-07 02:37:22 +0600

    pagination done

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Post = ({post}) => {
     return (
         <div >
-                    <div className="card bg-base-100 shadow-xl">
+                    <div className="card w-auto bg-base-100 shadow-xl">
                         <div className="card-body">
                             <h2 className="card-title">{post.title}</h2>
                             <p>{post.body}</p>
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Post from '../Components/Post';
 
+
 const Projects = () => {
     const [posts, setPosts] = useState([])
+    const [page, setPage] = useState(1)
 
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/posts")
@@ -11,19 +13,43 @@ const Projects = () => {
 
     }, [])
 
+    const pageHandler = (select) => {
+        if (select >= 1 && select <= posts.length / 10 && select !== page)
+            setPage(select)
+    }
+
     return (
         <div>
-           
+
             {
-                posts.map(post =>
-                    <div className='grid grid-cols-1 lg:grid-cols-4 gap-4 p-4'>
+                posts.slice(page * 10 - 10, page * 10).map(post =>
+                    <div className='grid grid-cols-4 gap-4 p-4'>
                         <Post
                             post={post}
                             key={post.id}
                         ></Post>
                     </div>)
             }
-           
+            {
+                posts.length > 0 && <div class="join">
+                    <button
+                        onClick={() => pageHandler(page - 1)}
+                        class="join-item btn">«</button>
+                    {
+                        [...Array(posts.length / 10)].map((_, i) => {
+                            return <button
+                                onClick={() => pageHandler(i + 1)}
+                                className={page === (i + 1) ? "join-item btn " : "join-item btn btn-active"}>
+                                Page {i + 1}
+                            </button>
+                        })
+                    }
+                    <button
+                        onClick={() => pageHandler(page + 1)}
+                        class="join-item btn">»</button>
+                </div>
+            }
+
         </div>
     );
 };
