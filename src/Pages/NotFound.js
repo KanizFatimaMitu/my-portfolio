cf0e55f474a0e5fd72883d7e20a3ba07dc100ef3
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-07-08 23:59:50 +0600

    fixed prob

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import  pic from '../Assests/404.jpg'
 
 const NotFound = () => {
     return (
         <div>
             <h1>404</h1>
+            <img src={pic} alt='picture' />
         </div>
     );
 };
